3e519df6330811896fafc3b4bb6e946af2971cf3
Snehae <snehathakur@navgurukul.orgm>
2026-05-28 09:33:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// SWIPER
+
+const swiper = new Swiper(".heroSwiper", {
+
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
